878ae1117cdee91a7d441122a64523aff6595efe
Lucy Verotia Swele <132388392+KhaniLucy@users.noreply.github.com>
2024-05-01 10:24:12 +0200

    added day 17 file
    Exception Handling

---
+++ 17-ExceptionHandling/exceptionhandling.py
@@ -0,0 +1,152 @@
+"""EXCEPTION HANDLING"""
+# python uses try and except to handle errors
+try:
+    print(10 + '5')
+except:
+    print('Something went wrong')
+# in this example the second opreand is a string. for it to work we will have to change it to float or int to add it with the number to make it work.
+# however, without any changes the second block, except, will be executed
+
+
+# example 2
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except:
+    print('Something went wrong')
+# the exception block will run and we do not know the exact problem. 
+# therefore, to analyze the problem, we can use the different error types with except.
+
+
+# this example, handles the error and tells us the kind of error being raised
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occured')
+except ValueError:
+    print('Value error occured')
+except ZeroDivisionError:
+    print('zero division error occured')
+# TypeError
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print('You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print('You are {name}. And your age is {age}.')
+except Exception as e:
+    print(e)
+
+
+'''Packing and Unpacking Arguments in Python'''
+# two operators are used: for tuples and for dictionaries
+
+'''Unpacking list'''
+def sum_of_five_nums(a, b, c, d, e):
+    return a + b + c + d + e
+
+lst = [1, 2, 3, 4, 5]
+print(sum_of_five_nums(lst)) # TypeError: sum_of_five_nums() missing 4 required positional arguments: 'b', 'c', 'd', and 'e'
+# running this code raises an error, because this function takes numbers(not a list)as arguments
+
+# unpacking/destructuring the list
+def sum_of_five_nums(a, b, c, d, e):
+    return a + b + c + d + e
+
+lst = [1, 2, 3, 4, 5]
+print(sum_of_five_nums(*lst))  # 15
+
+# using unpacking in the range built-in function that expects a start and an end
+numbers = range(2, 7)  # normal call with separate arguments
+print(list(numbers)) # [2, 3, 4, 5, 6]
+args = [2, 7]
+numbers = range(*args)  # call with arguments unpacked from a list
+print(numbers)      # [2, 3, 4, 5,6]
+
+
+# a list or tuple can be unpacked like this:
+countries = ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+fin, sw, nor, *rest = countries
+print(fin, sw, nor, rest)   # Finland Sweden Norway ['Denmark', 'Iceland']
+numbers = [1, 2, 3, 4, 5, 6, 7]
+one, *middle, last = numbers
+print(one, middle, last)      #  1 [2, 3, 4, 5, 6] 7
+
+# unpacking dictionaries
+def unpacking_person_info(name, country, city, age):
+    return f'{name} lives in {country}, {city}. He is {age} year old.'
+dct = {'name':'Asabeneh', 'country':'Finland', 'city':'Helsinki', 'age':250}
+print(unpacking_person_info(**dct)) # Asabeneh lives in Finland, Helsinki. He is 250 years old.
+
+
+'''Packing'''
+# sometimes its never known how many arguments need to be passed to a pyhton function
+
+#Packing Lists
+def sum_all(*args):
+    s = 0
+    for i in args:
+        s += i
+    return s
+print(sum_all(1, 2, 3))             # 6
+print(sum_all(1, 2, 3, 4, 5, 6, 7)) # 28
+
+# Packing Dictionaries
+def packing_person_info(**kwargs):
+    # check the type of kwargs and it is a dict type
+    # print(type(kwargs))
+    # Printing dictionary items
+    for key in kwargs:
+        print(f"{key} = {kwargs[key]}")
+    return kwargs
+
+print(packing_person_info(name="Asabeneh",
+      country="Finland", city="Helsinki", age=250))
+
+
+'''Spreading In Python'''
+# like in JavaScript, spreading is possible in python
+lst_one = [1, 2, 3]
+lst_two = [4, 5, 6, 7]
+lst = [0, *lst_one, *lst_two]
+print(lst)          # [0, 1, 2, 3, 4, 5, 6, 7]
+country_lst_one = ['Finland', 'Sweden', 'Norway']
+country_lst_two = ['Denmark', 'Iceland']
+nordic_countries = [*country_lst_one, *country_lst_two]
+print(nordic_countries)  # ['Finland', 'Sweden', 'Norway', 'Denmark', 'Iceland']
+
+
+'''Enumerate'''
+# we use enumerate built-in function to get the index of each item in the list
+for index, item in enumerate([20, 30, 40]):
+    print(index, item)
+
+for index, i in enumerate(countries):
+    print('hi')
+    if i == 'Finland':
+        print('The country {i} has been found at index {index}')
+    
+
+'''Zip'''
+# we use this method to combine lists when looping through them
---
+++ 17-ExceptionHandling/try_block.py
@@ -0,0 +1,17 @@
+"""Need for exception Handling"""
+# the program terminates whenever an error occurs
+# sudden termination can corrupt the program
+# exception may cause data loss
+'''helps to have a clean program even when execution of the program happens it thows errors'''
+
+
+'''Exception: is a runtime error that terminates the execution of a program'''
+
+'''Execption handling'''
+# generates a message in correspondence to the occurrence of an error and takes the right step to handle it
+
+'''three types of errors'''
+# compile time error
+# logical errors
+# run time errors
+
